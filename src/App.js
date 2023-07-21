614f74dc24c9a632f93c3645c6ef5ccb3f22ccc6
Daniel Suarez <est.daniel.suarez1@unimilitar.edu.co>
2023-07-21 15:06:33 -0500

    Counter y filtro agregado

--- src/App.js
+++ src/App.js
@@ -14,13 +14,27 @@ const defaultTodos = [
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+  
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+  
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase()
+    return todoText.includes(searchText)
+  });
   return (
     <React.Fragment>
-      <TodoCounter    completed={14} total={25}/>
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos}/>
+      <TodoSearch 
+        searchValue = {searchValue}
+        setSearchValue = {setSearchValue}
+      />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem key={todo.text} texto ={todo.text} completado = {todo.completed}/>
         ))}
       </TodoList>
