8262a51e4d17601487b40b29f2dd9d37af66524c
Daniel Suarez <est.daniel.suarez1@unimilitar.edu.co>
2023-07-21 22:18:03 -0500

    mensaje por completar todos los TODOs

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,6 +2,14 @@ import './TodoCounter.css';
 
 function TodoCounter({ total, completed }){
     return(
+      total == completed & total != 0 ?
+      
+      <h1 className='counter'>
+        Felicidades por completar los TODOs
+      </h1>
+      
+      :
+
       <h1 className='counter'>
         Has completado {completed} de {total} TODOs
       </h1>
