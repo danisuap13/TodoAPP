cbc9c511d6c659a8cdad2b958b3f04104cc386b5
Daniel Suarez <est.daniel.suarez1@unimilitar.edu.co>
2023-07-31 16:59:59 -0500

    Modal

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -6,22 +7,27 @@ import { TodosLoading } from '../TodosLoading';
 import { TodosError } from '../TodosError';
 import { EmptyTodos } from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
 import { TodoContext } from '../TodoContext';
 
 function AppUI() {
+  const {
+    loading,
+    error,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal
+  } = React.useContext(TodoContext)
+
   return (
     <>
       <TodoCounter />
-      <TodoSearch />
+      <TodoSearch /> 
 
       <TodoContext.Consumer>
-        {({
-          loading,
-          error,
-          searchedTodos,
-          completeTodo,
-          deleteTodo,
-        }) => (
+        {() => (
           <TodoList>
             {loading && (
               <>
@@ -47,6 +53,13 @@ function AppUI() {
       </TodoContext.Consumer>
       
       <CreateTodoButton />
+              
+      {openModal && (
+        <Modal>
+          Usando REact Portals
+        </Modal>
+      )
+      }
     </>
   );
 }
